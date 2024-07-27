eb19935e5ba05c231888793caf2726ee92e8294b
jewestyes <cool.kriss99@yandex.ru>
2024-07-27 22:53:25 +0300

    Добавлена реализация отправки данных с формы регистрации в БД

--- Models/StatItem.cs
+++ Models/StatItem.cs
@@ -1,4 +1,6 @@
-﻿namespace VlasikhaPlavanieWebsite.Models
+﻿using System.Collections.Generic;
+
+namespace VlasikhaPlavanieWebsite.Models
 {
     public class StatItem
     {
@@ -6,6 +8,6 @@
         public string Date { get; set; }
         public string Name { get; set; }
         public string City { get; set; }
-        public List<string> Files { get; set; }
+        public List<string> Files { get; set; } = new List<string>();
     }
 }
