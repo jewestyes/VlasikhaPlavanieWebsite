e19c3f551c9d6330a33df1252ca403290139783d
jewestyes <cool.kriss99@yandex.ru>
2024-08-08 22:58:36 +0300

    Заменена инфо по платежному банку. В регистрации добавлены ограничения по выбираемым дистанциям

--- wwwroot/js/registration.js
+++ wwwroot/js/registration.js
@@ -9,20 +9,39 @@
     };
 
     /**
+     * Получает год рождения участника.
+     * @param {HTMLElement} participantSection - секция участника.
+     * @returns {number} - год рождения.
+     */
+    function getBirthYear(participantSection) {
+        const birthDateInput = participantSection.querySelector('input[type="date"]');
+        if (birthDateInput && birthDateInput.value) {
+            return new Date(birthDateInput.value).getFullYear();
+        }
+        return null;
+    }
+
+    /**
      * Обновляет список дистанций для выбранной дисциплины.
      * @param {HTMLElement} selectElement - селект дисциплины.
      */
     function updateDistanceOptions(selectElement) {
         const selectedDiscipline = selectElement.value;
-        const distanceSelect = selectElement.closest('.discipline-section').querySelector('.distance-select');
+        const disciplineSection = selectElement.closest('.discipline-section');
+        const distanceSelect = disciplineSection.querySelector('.distance-select');
         const currentDistance = distanceSelect.getAttribute('data-current-distance');
+        const participantSection = disciplineSection.closest('.participant-section');
+        const birthYear = getBirthYear(participantSection);
 
         // Очистка текущих опций
         distanceSelect.innerHTML = '';
 
-        // Добавление новых опций
+        // Добавление новых опций с учетом года рождения
         if (disciplineOptions[selectedDiscipline]) {
             disciplineOptions[selectedDiscipline].forEach(distance => {
+                if ((distance === "25м" && (selectedDiscipline === "На спине" || selectedDiscipline === "Вольный стиль")) && (birthYear < 2016 || birthYear > 2018)) {
+                    return; // Пропустить опцию, если год рождения не в диапазоне 2016-2018
+                }
                 const option = document.createElement('option');
                 option.value = distance;
                 option.text = distance;
@@ -42,6 +61,15 @@
             updateDistanceOptions(selectElement);
             selectElement.addEventListener('change', function () {
                 updateDistanceOptions(this);
+            });
+        });
+
+        document.querySelectorAll('input[type="date"]').forEach(dateInput => {
+            dateInput.addEventListener('change', function () {
+                const participantSection = this.closest('.participant-section');
+                participantSection.querySelectorAll('.discipline-select').forEach(selectElement => {
+                    updateDistanceOptions(selectElement);
+                });
             });
         });
     }
