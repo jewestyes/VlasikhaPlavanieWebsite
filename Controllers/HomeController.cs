a4629f7e9305b4451621aebc07c325a4f7afda64
jewestyes <cool.kriss99@yandex.ru>
2024-09-05 21:12:11 +0300

    Добавлен функционал в админ панель для работы с файлами

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using VlasikhaPlavanieWebsite.Data;
 using VlasikhaPlavanieWebsite.Models;
 
 namespace VlasikhaPlavanieWebsite.Controllers
@@ -7,18 +11,40 @@ namespace VlasikhaPlavanieWebsite.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
-        {
-            return View();
+		public async Task<IActionResult> Index()
+		{
+            try
+            {
+                var fileMappings = await _context.FileMappings
+                    .Select(f => new
+                    {
+                        f.ButtonName,
+                        FilePath = f.FilePath ?? "#"
+                    })
+                    .ToListAsync();
+
+                var buttonFiles = fileMappings.ToDictionary(
+                    f => f.ButtonName,
+                    f => f.FilePath
+                );
+                return View(buttonFiles);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"[HomeController] [Index] ERROR: {ex.Message}");
+            }
+            return View(new Dictionary<string,string>());
         }
 
-        public IActionResult Registration()
+		public IActionResult Registration()
         {
             return View();
         }
---
+++ Models/FileMapping.cs
@@ -0,0 +1,10 @@
+﻿namespace VlasikhaPlavanieWebsite.Models
+{
+    public class FileMapping
+    {
+        public int Id { get; set; }
+        public string ButtonName { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+    }
+}
