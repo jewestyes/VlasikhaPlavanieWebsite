99b5a968572c789537ecdd0d886c0d9a8d278b90
jewestyes <cool.kriss99@yandex.ru>
2025-10-19 15:17:33 +0300

    fix

--- src/VlasikhaPlavanieWebsite.Api/Controllers/TinkoffController.cs
+++ src/VlasikhaPlavanieWebsite.Api/Controllers/TinkoffController.cs
@@ -18,7 +18,7 @@ namespace VlasikhaPlavanieWebsite.Controllers
 			_webhookService = webhookService;
 		}
 		[HttpPost("webhook")]
-		public async Task<IActionResult> AlfaWebhook([FromBody] TinkoffWebhook model)
+		public async Task<IActionResult> TinkoffWebhook([FromBody] TinkoffWebhook model)
 		{
 			try
 			{
